d0ceb4ee685b6ef04e127c2eb5d72c2d477ca0fd
Leonardo Borges <79729169+JLeoBorges@users.noreply.github.com>
2023-01-19 13:00:50 -0300

    Add files via upload

--- cursotestes.php
+++ cursotestes.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Estudos</title>
+    <h1>Estudos de PHP</h1>
 </head>
 <body>
     
 <?php
-    echo "hello, world!"; 
+    echo "hello world!"; 
 ?>    
 
 </body>
