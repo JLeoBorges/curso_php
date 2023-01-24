966cf4b4cb5b8cf7d6c26218fda07b24ab46bc4a
Leonardo Borges <79729169+JLeoBorges@users.noreply.github.com>
2023-01-24 12:52:38 -0300

    Add files via upload

---
+++ exercicios.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="br">
+<head>
+    <meta charset="UTF-8">
+    <title>Estudos</title>
+    <h1>Exercícios</h1>
+</head>
+<body>
+    
+<?php    
+                        //exercício 1 operador unário //
+    // colocar valores na url exemplo.php"?x=1&y=2"//
+    //$varx = $_GET["x"];
+    //$vary = $_GET["y"];
+    //$op = $_GET["z"];
+    //$maior = $varx>$vary ? $varx:$vary;
+//echo "</br>O maior valor da operação Unária é: $maior"; 
+
+    //$operacao = ($op == "soma")? $varx+$vary : $varx-$vary ;   
+    //echo "</br> O Valor da operação : $operacao"
+
+
+
+                        //exercício 2 //
+    //$av1 = $_GET["n1"];
+    //$av2 = $_GET["n2"];
+    //$media = ($av1+$av2) /2;
+    //echo "resultado da media: $media<br/>";
+    //echo "resultado : ".(($media>=7)?"APROVADO":"REPROVADO");
+
+
+
+
+
+?>    
+
+</body>
+</html>
