048b089feccc8b55c0127e924a6582de097b2e2b
Leonardo Borges <79729169+JLeoBorges@users.noreply.github.com>
2023-01-24 11:22:21 -0300

    Add files via upload

--- op_unario.php
+++ op_unario.php
@@ -7,12 +7,18 @@
 </head>
 <body>
     
-<?php
+<?php    
     // operador unário //
+    // colocar valores na url exemplo.php"?x=1&y=2"//
     $varx = $_GET["x"];
     $vary = $_GET["y"];
+    $op = $_GET["z"];
     $maior = $varx>$vary ? $varx:$vary;
 echo "</br>O maior valor da operação Unária é: $maior"; 
+
+    $operacao = ($op == "soma")? $varx+$vary : $varx-$vary ;   
+    echo "</br> O Valor da operação : $operacao"
+
 ?>    
 
 </body>
