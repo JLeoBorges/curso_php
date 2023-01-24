580beb7a56a026cd6ab73bbb74e2e8b305022930
Leonardo Borges <79729169+JLeoBorges@users.noreply.github.com>
2023-01-24 10:08:43 -0300

    Add files via upload

---
+++ op_unario.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="br">
+<head>
+    <meta charset="UTF-8">
+    <title>Estudos</title>
+    <h1>Operador Unário</h1>
+</head>
+<body>
+    
+<?php
+    // operador unário //
+    $varx = $_GET["x"];
+    $vary = $_GET["y"];
+    $maior = $varx>$vary ? $varx:$vary;
+echo "</br>O maior valor da operação Unária é: $maior"; 
+?>    
+
+</body>
+</html>
