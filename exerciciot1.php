b1209b109fcd5b787cede36d36b712eda9989f12
Leonardo Borges <79729169+JLeoBorges@users.noreply.github.com>
2023-01-30 16:31:59 -0300

    Add files via upload

---
+++ exerciciot1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <?php
+        $txt = isset ($_GET["t"]) ? $_GET["t"] : "Texto Genérico"; 
+        $tam = isset ($_GET["tam"]) ? $_GET["tam"] : "10pt"; 
+        $cor = isset ($_GET["cor"]) ? $_GET["cor"] : "#000000";
+    ?>
+        <style>
+            span.texto{
+                font-size: <?php echo $tam; ?>;
+                color: <?php echo $cor; ?>;
+            }
+            </style>
+
+    <meta charset="UTF-8">
+    <title>exercícioT1</title>
+</head>
+<body>
+    <div>
+        <?php
+
+            echo "<span class ='texto'> $txt </span> <br/>" ;
+                    
+        ?>
+        <a href= "exerciciot1.html" >Retornar </a>
+
+    </div>
+
+
+
+</body>
+</html>
