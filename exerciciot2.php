c66a2133740fcf3b29fe5094f17f3b50a780c9f3
Leonardo Borges <79729169+JLeoBorges@users.noreply.github.com>
2023-02-02 14:53:53 -0300

    Add files via upload

---
+++ exerciciot2.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="br">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <?php
+            $a = isset ($_GET["ano"])?$_GET["ano"] : "não informado";
+            if($a == "não informado")
+                    {echo "Idade Não informada";}
+            else
+            {
+            $i = date("Y") - $a; 
+            echo "A sua idade é: $i";
+                if($i >= 18 && $i <=65){
+                    $v = "obrigação com o voto";
+                    $d = "apitidão a digirir";
+                }
+                else{
+                    $v = "nenhuma obrigatoriedade com voto";
+                    $d = "que verificar mais informações sobre a posse da CNH";
+                }                
+                              
+            }
+            echo "</br>Você tem $v e $d"; 
+        ?>
+</div>
+</body>
+</html>
