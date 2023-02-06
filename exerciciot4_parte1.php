affda2ec7507416b503c3d7e9466d4807d82105e
Leonardo Borges <79729169+JLeoBorges@users.noreply.github.com>
2023-02-06 15:02:32 -0300

    Add files via upload

---
+++ exerciciot4_parte1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="br">
+<head>
+    <meta charset="UTF-8">
+    <title>exercício de repetição</title>
+</head>
+<body>
+    <div>
+        <form method="get" action="exerciciot4_parte2.php">
+            <?php
+            $c = 1;
+            while ($c <= 5){
+            echo "Valor $c : <input type='number' name='v1' min='0' max='100' value='0'/><br/>";
+            $c++;
+        
+        }
+            ?>
+
+            <input type="submit" value="Enviar" class="botao"/>
+    </div>   
+    
+</body>
+</html>
