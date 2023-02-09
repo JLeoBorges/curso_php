d9a120be044ac048df8613dfb974b3ade209ac2d
Leonardo Borges <79729169+JLeoBorges@users.noreply.github.com>
2023-02-09 14:25:21 -0300

    Add files via upload

---
+++ exerciciot6_parte2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="br">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <?php
+        $n = isset($_GET["num"])?$_GET["num"]:1;
+        for($c=1;$c<=10;$c++){
+            $r = $n * $c;
+            echo "$n x $c = $r<br/>";
+        }
+        
+        ?>
+
+        <p><a href="exerciciot6_parte1.php">Retornar</a></p>
+</div>
+    
+</body>
+</html>
