56afb5095256f17ec501be2c60824d77494a9a74
Leonardo Borges <79729169+JLeoBorges@users.noreply.github.com>
2023-02-18 20:52:57 -0300

    Add files via upload

--- exerciciot9.php
+++ exerciciot9.php
@@ -39,7 +39,10 @@
         $vet = str_split($meunick);
         print_r($vet);
 
-        echo "</br></br></br></br></br>";
+        echo "</br></br></br>";
+        print(str_repeat("-",400));
+        echo "</br></br></br>";
+        
         $vetor3[0]= "mucho";
         $vetor3[1]= "texto";
         $texto= join(" ",$vetor3);
@@ -51,12 +54,45 @@
         $cod = ord($letra);
         print("o código de $letra é $cod");
 
+        echo "</br></br></br></br></br>";
+        $nome = "Teste InsirA um NomE mais UM Teste";
+         $nome2 = ucwords(strtolower($nome));
+         echo "Meu nome é: $nome2";
+         print("</br>meu nome é : ".strtolower($nome));
+         print("</br>meu nome é : ".strtoupper($nome));
+
+         print("</br>Nome ao contrário: ");
+         print(strrev($nome));
 
 
+         echo "</br></br></br></br></br>";
+        $pos = stripos($nome, "UM");
+        echo "$nome </br>posição do nome é : $pos";
 
 
+        $cont = substr_count($nome, "Teste");
+        Print("</br>InsirA foi encontrado $cont vez(es)");
+
+        $sub = substr($nome, 0, 12);
+        echo "</br> $sub";
+
+        echo "</br></br></br></br></br>";
+        /*$espaçamento = "espaçamento";
+        $novoespa = str_pad($espaçamento, 40, " ",STR_PAD_LEFT);
+        $newespa = str_replace(" ", "  .", $novoespa);
+        print("</br></br>Aqui NÃO tem um $newespa até aqui");
+        */
+
+        echo "</br>";
+        print(str_repeat("-",400));
+        echo "</br>";
+        $umtxt = str_repeat("Repetindo", 7);
+        print("</br>$umtxt");
+
+        
+        
 
 ?>  
-     
+
 </body>
 </html>
