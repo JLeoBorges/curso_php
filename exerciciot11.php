de94ec07b305d356b00badf9b27364131a1d3896
Leonardo Borges <79729169+JLeoBorges@users.noreply.github.com>
2023-02-19 20:40:33 -0300

    Add files via upload

---
+++ exerciciot11.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Aula Matrizes</title>
+</head>
+<body>
+    <?php
+
+    $n = array( array(2,3),
+                array(3,4),
+                array(7,7));
+
+    $n [0][1] = $n [2][1];
+    print_r($n);
+
+
+    ?>
+    
+</body>
+</html>
